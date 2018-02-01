1b4374ad95fb7cacae6202e7defd9b58bfd24efa
Medinilla <cris.medinill@gmail.com>
2018-02-01 16:35:42 -0600

    Add room to config file & Update template

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,11 +1,10 @@
+const fs  = require('fs');
+const json = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
 module.exports = function(robot) {
     robot.respond(/feedback (.*)/i, function(msg){
         var today = new Date();
         feedback = msg.match[1];
-        robot.messageRoom('G91QEK883',  `@here Hi leadership team i received new feedback: ${feedback}`);
-    });
-
-    robot.hear(/(mexico || México || Mexico)/i,function(msg){
-        msg.send('mexico rocks');
+        robot.messageRoom(json.roomId,  `@here Hi leadership team i received new feedback: ${feedback}`);
     });
 }
