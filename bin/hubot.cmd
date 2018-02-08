0c520f7323a0a7210d40b4614bee26daf0addb6b
Irving Gomez <IrvingGomez@DESKTOP-GSRCHIR>
2018-02-08 12:22:44 -0600

    Update hubot.cmd to include adapter and token

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -3,5 +3,7 @@
 call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+SET HUBOT_ADAPTER=slack
+SET HUBOT_SLACK_TOKEN=fill-please
 
 node_modules\.bin\hubot.cmd --name "intersys-mdc-hubot" %* 
